db244118dd2d8ed5bb0660b84fda35b0ee79f753
Julian Sanin <j54n1n+noreply@gmail.com>
2018-03-31 20:11:01 +0100

    Updated cpprestsdk build script to support Debian/Raspbian Stretch

--- libs/build_dependencies.sh
+++ libs/build_dependencies.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-RESTSDK_VERSION="v2.9.1"
+RESTSDK_VERSION="v2.10.2"
 DEFAULT_LIB_DIRECTORY_PATH="."
+BUILD_PACKAGES="build-essential git cmake zlib1g-dev libboost-all-dev libssl1.0-dev"
 
 libDir=${1:-$DEFAULT_LIB_DIRECTORY_PATH}
+
+install_build_deps() {
+  sudo apt update
+  sudo apt install --yes $BUILD_PACKAGES
+}
 
 install_cpprestsdk(){
 	restsdkDir="$libDir/cpprestsdk"
@@ -16,12 +22,10 @@ install_cpprestsdk(){
 	git clone https://github.com/Microsoft/cpprestsdk.git "$restsdkDir"
 	(cd $restsdkDir && git checkout tags/$RESTSDK_VERSION -b $RESTSDK_VERSION)
 	mkdir "$restsdkBuildDir"
-	if [[ "$OSTYPE" == "linux-gnu" ]]; then
-		export CXX=g++-4.9
-	fi
 	(cd "$restsdkBuildDir" && cmake ../Release -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTS=OFF -DBUILD_SAMPLES=OFF)
-	(cd "$restsdkBuildDir" && make)
+	(cd "$restsdkBuildDir" && make -j$(nproc))
 }
 
 mkdir -p "$libDir"
+install_build_deps
 install_cpprestsdk
